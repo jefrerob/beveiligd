f59fca23fe01b14a77a9fa948189a8ccb123b5d2
jefrerob <59914947+jefrerob@users.noreply.github.com>
2021-02-08 19:03:03 +0100

    p24

---
+++ src/main/java/be/vdab/beveiligd/controllers/WerknemersController.java
@@ -0,0 +1,16 @@
+package be.vdab.beveiligd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("werknemers")
+class WerknemersController {
+
+    @GetMapping
+    public String werknemers() {
+        return "werknemers";
+    }
+
+}
