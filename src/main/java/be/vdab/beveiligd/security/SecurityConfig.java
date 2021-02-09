2fede391931aad2f81ab79de6114fb990c74e5c4
JefreRobert <jefre.robert@vdabcampus.be>
2021-02-09 12:26:18 +0100

    p 29

--- src/main/java/be/vdab/beveiligd/security/SecurityConfig.java
+++ src/main/java/be/vdab/beveiligd/security/SecurityConfig.java
@@ -6,24 +6,25 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String MANAGER = "manager";
     private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
     private static final String MAGAZIJNIER = "magazijnier";
+    private final DataSource datasource;
+
+    public SecurityConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth
-                .inMemoryAuthentication()
-                .withUser("joe")
-                .password("{noop}theboss")
-                .authorities(MANAGER)
-                .and()
-                .withUser("averell")
-                .password("{noop}hungry")
-                .authorities(HELPDESKMEDEWERKER, MAGAZIJNIER);
+    protected void
+    configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.jdbcAuthentication().dataSource(datasource);
     }
+
     @Override public void configure(WebSecurity web) {
 
         web
@@ -32,12 +33,16 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/css/**")
                 .mvcMatchers("/js/**");
     }
-    @Override protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
-            http
-                    .authorizeRequests(requests -> requests .mvcMatchers("/offertes")
-                        .hasAuthority(MANAGER)
-                        .mvcMatchers("/werknemers")
-                        .hasAnyAuthority(MAGAZIJNIER,HELPDESKMEDEWERKER));
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.logout(logout -> logout.logoutSuccessUrl("/"));
+        http.formLogin(login -> login.loginPage("/login"));
+        http
+                    .authorizeRequests(requests -> requests
+                            .mvcMatchers("/offertes").hasAuthority(MANAGER)
+                        .mvcMatchers("/werknemers").hasAnyAuthority(MAGAZIJNIER,HELPDESKMEDEWERKER)
+                            .mvcMatchers("/", "/login").permitAll()
+                            .mvcMatchers("/**").authenticated());
     }
 }
